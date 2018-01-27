0edd96fb7b94b05f136b95c3195f24f2d6b1b6da
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-01-27 11:48:43 -0600

    Lineage -> Invictrix

--- invictrix.mk
+++ invictrix.mk
@@ -1,26 +1,21 @@
-# Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+$(call inherit-product, vendor/invictrix/config/common.mk)
+$(call inherit-product, vendor/invictrix/config/gsm.mk)
+
 # Vendor blobs
-$(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
+$(call inherit-product, vendor/essential/mata/mata-vendor.mk)
 
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
-# TWRP
-ifeq ($(WITH_TWRP),true)
-$(call inherit-product, device/essential/mata/twrp/twrp.mk)
-else
 TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
-endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := invictrix_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
